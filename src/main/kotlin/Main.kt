f154c5a210cce9813e0e19a3dba25251a1f0940b
KimA-Mik <ananim38@gmail.com>
2024-09-10 21:09:30 +0300

    Braces

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -21,8 +21,12 @@ fun main(args: Array<String>) {
         when (c) {
             '(' -> println("LEFT_PAREN ( null")
             ')' -> println("RIGHT_PAREN ) null")
-            '{'-> println("LEFT_BRACE { null")
+            '{' -> println("LEFT_BRACE { null")
             '}' -> println("RIGHT_BRACE } null")
+            '*' -> println("STAR * null")
+            '.' -> println("DOT . null")
+            ',' -> println("COMMA , null")
+            '+' -> println("PLUS + null")
             else -> continue
         }
     }
