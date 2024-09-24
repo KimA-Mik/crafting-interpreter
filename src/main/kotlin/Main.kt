2f2977a2a6a9749e4b386eaf22af4030fd8483a8
KimA-Mik <ananim38@gmail.com>
2024-09-24 16:51:03 +0300

    Booleans & Nil

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,11 +10,14 @@ fun main(args: Array<String>) {
     val command = args[0]
     val filename = args[1]
 
-    if (command != "tokenize") {
-        System.err.println("Unknown command: ${command}")
-        exitProcess(1)
+    when (command) {
+        "tokenize" -> tokenize(filename)
+        "parse" -> parse(filename)
+        else -> unknownCommand(command)
     }
+}
 
+private fun tokenize(filename: String) {
     val fileContents = File(filename).readText()
 
     val lexer = Lexer(fileContents)
@@ -27,4 +30,29 @@ fun main(args: Array<String>) {
     if (lexer.lexicalError) {
         exitProcess(65)
     }
+}
+
+private fun parse(filename: String) {
+    val fileContents = File(filename).readText()
+
+    val lexer = Lexer(fileContents)
+    val tokens = lexer.tokenise()
+
+    tokens.forEach {
+        when (it.type) {
+            TokenType.FALSE -> println(it.lexeme)
+            TokenType.NIL -> println(it.lexeme)
+            TokenType.TRUE -> println(it.lexeme)
+            else -> {}
+        }
+    }
+
+    if (lexer.lexicalError) {
+        exitProcess(65)
+    }
+}
+
+private fun unknownCommand(command: String) {
+    System.err.println("Unknown command: $command")
+    exitProcess(1)
 }
